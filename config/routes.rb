66992aa6b4abf764d5ec4ff59f73a08792fd6ce5
rypilu <rpiluski@gmail.com>
2022-04-19 06:26:45 -0400

    added article list feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # this is app/controllers/application_controller.rb -- rails understands application to be application_controller.rb
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index]
 end
